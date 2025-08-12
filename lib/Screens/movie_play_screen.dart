47267091c7497076e2b5d55770047ed5b88671c0
anuj-1637 <anuj94460@gmail.com>
2025-08-12 15:30:21 +0530

    Movies App using API

--- lib/Screens/movie_play_screen.dart
+++ lib/Screens/movie_play_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../API/movie_api.dart';
 import '../model/discover_movie_model.dart';
@@ -38,27 +39,63 @@ class Movies_Play_ScreeenState extends State<Movies_Play_Screeen> {
               snapshot.data![1].results ?? [] as List<discoverMovie>;
 
           late String imagurl;
+          late String title;
+          late String description;
+          late String popularity;
           if (widget.listName == "Latest Movies") {
             imagurl =
                 "https://image.tmdb.org/t/p/w500${movies[widget.index].posterPath}";
+            title = movies[widget.index].title!;
+            description = movies[widget.index].overview!;
+            popularity = movies[widget.index].popularity!.toStringAsFixed(1);
           } else if (widget.listName == "Popular Movies") {
             imagurl =
                 "https://image.tmdb.org/t/p/w500${discoverMovies[widget.index].posterPath}";
+            title = discoverMovies[widget.index].title!;
+            description = discoverMovies[widget.index].overview!;
+            popularity = discoverMovies[widget.index].popularity!
+                .toStringAsFixed(1);
           } else if (widget.listName == "Top Rated Movies") {
             imagurl =
                 "https://image.tmdb.org/t/p/w500${movies[widget.index].posterPath}";
+            title = movies[widget.index].title!;
+            description = movies[widget.index].overview!;
+            popularity = movies[widget.index].popularity!.toStringAsFixed(1);
           }
 
-          return Hero(
-            tag: "Movies",
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Hero(
+                tag: "Movies",
 
-            transitionOnUserGestures: true,
-            child: Image.network(
-              imagurl,
-              width: double.infinity,
-              height: 200,
-              fit: BoxFit.fill,
-            ),
+                transitionOnUserGestures: true,
+                child: Image.network(
+                  imagurl,
+                  width: double.infinity,
+                  height: 200,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0.w),
+                child: Text('$title', style: TextStyle(fontSize: 18.sp)),
+              ),
+              // SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('$description', style: TextStyle(fontSize: 14.sp)),
+              ),
+              // SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Popularity: $popularity',
+                  style: TextStyle(fontSize: 14.sp),
+                ),
+              ),
+            ],
           );
         },
       ),
